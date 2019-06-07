dc0ba095baab752fe8ce564eb9d9eacb375c086c
AlexxCh <sancho.ventura95@gmail.com>
2019-06-07 13:59:25 +0300

    Update app.js

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,7 +2,7 @@
 import "../stylesheets/app.css";
 
 // Import libraries we need.
-import { default as Web3} from 'web3';
+//import { default as Web3} from 'web3';
 import { default as contract } from 'truffle-contract';
 import { default as ethUtil} from 'ethereumjs-util';
 import { default as sigUtil} from 'eth-sig-util';
@@ -86,10 +86,11 @@ window.voteForCandidate = function(candidate) {
 }
 
 $( document ).ready(function() {
+  let Web3 = require('web3');
   if (typeof web3 !== 'undefined') {
     console.warn("Using web3 detected from external source like Metamask")
     // Use Mist/MetaMask's provider
-    window.web3 = new Web3(web3.currentProvider);
+    web3 = new Web3(web3.currentProvider);
   } else {
     console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
