3bfe2303a9464810fe39866b1fda1102a5882466
AlexxCh <sancho.ventura95@gmail.com>
2019-06-07 15:25:50 +0300

    1

--- index.js
+++ index.js
@@ -236,14 +236,14 @@ signTypedDataButton.addEventListener('click', function(event) {
     }
     if (result.error) return console.error(result)
     console.log('PERSONAL SIGNED:' + JSON.stringify(result.result))
-
+    $("#sign").html = JSON.stringify(result.result)
     const recovered = sigUtil.recoverTypedSignatureLegacy({ data: msgParams, sig: result.result })
 
-    if (ethUtil.toChecksumAddress(recovered) === ethUtil.toChecksumAddress(from)) {
+    /*if (ethUtil.toChecksumAddress(recovered) === ethUtil.toChecksumAddress(from)) {
       alert('Successfully ecRecovered signer as ' + JSON.stringify(result.result))
     } else {
       alert('Failed to verify signer when comparing ' + result + ' to ' + from)
-    }
+    }*/
 
   })
 
