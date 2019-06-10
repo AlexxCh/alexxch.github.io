e2d08ed1b0cd1c9427df0ef07066b451bcc9fda6
AlexxCh <sancho.ventura95@gmail.com>
2019-06-10 11:39:55 +0300

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -7158,7 +7158,7 @@ ethjsPersonalSignButton.addEventListener('click', function(event) {
 })
 
 
-signTypedDataButton.addEventListener('click', function(event) {
+/*signTypedDataButton.addEventListener('click', function(event) {
   //var keccak256 = require('js-sha3').keccak_256; // eslint-disable-line
   event.preventDefault()
   let hotWallet = $("#hotWallet").val();
@@ -7191,15 +7191,6 @@ signTypedDataButton.addEventListener('click', function(event) {
   var from = web3.eth.accounts[0]
   if (!from) return connect()
 
-  /*  web3.eth.signTypedData not yet implemented!!!
-   *  We're going to have to assemble the tx manually!
-   *  This is what it would probably look like, though:
-    web3.eth.signTypedData(msg, from) function (err, result) {
-      if (err) return console.error(err)
-      console.log('PERSONAL SIGNED:' + result)
-    })
-  */
-
    console.log('CLICKED, SENDING PERSONAL SIGN REQ')
   var params = [msgParams, from]
   console.dir(params)
@@ -7223,7 +7214,49 @@ signTypedDataButton.addEventListener('click', function(event) {
       alert('Successfully ecRecovered signer as ' + from)
     } else {
       alert('Failed to verify signer when comparing ' + result + ' to ' + from)
-    }*/
+    }
+
+  })
+
+})*/
+
+signTypedDataButton.addEventListener('click', function(event) {
+  event.preventDefault()
+
+  const msgParams = [
+    {
+      type: 'string',
+      name: 'Message',
+      value: 'Hi, Alice!'
+    },
+    {
+      type: 'uint32',
+      name: 'A number',
+      value: '1337'
+    }
+  ]
+
+  var from = web3.eth.accounts[0]
+  if (!from) return connect()
+
+   console.log('CLICKED, SENDING PERSONAL SIGN REQ')
+  var params = [msgParams, from]
+  console.dir(params)
+  var method = 'eth_signTypedData'
+
+  web3.currentProvider.sendAsync({
+    method,
+    params,
+    from,
+  }, function (err, result) {
+    if (err) return console.dir(err)
+    if (result.error) {
+      alert(result.error.message)
+    }
+    if (result.error) return console.error(result)
+    console.log('PERSONAL SIGNED:' + JSON.stringify(result.result))
+    $("#sign").html("Signature: " + result.result);
+    const recovered = sigUtil.recoverTypedSignatureLegacy({ data: msgParams, sig: result.result })
 
   })
 
