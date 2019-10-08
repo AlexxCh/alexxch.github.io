9f19cba557b575f7defbb185fff561301316b9e2
AlexxCh <sancho.ventura95@gmail.com>
2019-10-08 16:34:33 +0300

    1

--- integration.js
+++ integration.js
@@ -1,6 +1,6 @@
 
 let Web3 = require('web3');
-
++
 	var MAINET_RPC_URL = 'https://mainnet.infura.io/v3/66492ced9c334deeb2bf9cd40f4e09b0' ;
 	var ROPSTEN_RPC_URL = 'https://ropsten.infura.io/v3/66492ced9c334deeb2bf9cd40f4e09b0' ;
 	var KOVAN_RPC_URL = 'https://kovan.infura.io/v3/66492ced9c334deeb2bf9cd40f4e09b0' ;
