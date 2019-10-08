5eadb48221a5121209728675822f418d2ab7c4d7
AlexxCh <sancho.ventura95@gmail.com>
2019-10-08 13:40:56 +0300

    Update integration.js

--- integration.js
+++ integration.js
@@ -268,7 +268,7 @@ var MyContract = web3.eth.contract(abi);
 var myContractInstance = MyContract.at('0x920f6aF3F0B36Da0565707207ec5E54c84257c3e');
 var result = myContractInstance.totalSupply();
 console.log(result) // '0x25434534534'
-var event = myContractInstance.Transfer({value: 10000}, {fromBlock: 0, toBlock: 'latest'});
+var event = myContractInstance.Transfer({value: 100000}, {fromBlock: 0, toBlock: 'latest'});
 // watch for changes
 event.watch(function(error, result){
  if (!error)
