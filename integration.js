7c256858a3ad628def928498a4d57b6db75898fb
AlexxCh <sancho.ventura95@gmail.com>
2019-10-09 13:58:08 +0300

    Update integration.js

--- integration.js
+++ integration.js
@@ -291,7 +291,7 @@ myEvent.watch(function(error, result){
 // would stop and uninstall the filter
 //myEvent.stopWatching();*/
 
-counter = web3.eth.contract(abi).at('0x920f6aF3F0B36Da0565707207ec5E54c84257c3e');
+let counter = web3.eth.contract(abi).at('0x920f6aF3F0B36Da0565707207ec5E54c84257c3e');
 
 counter.Transfer(function (err, result) {
   if (err) {
