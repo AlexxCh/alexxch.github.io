08c95be4cbeba44624f1cf1e629eee6e1f7d8088
AlexxCh <sancho.ventura95@gmail.com>
2019-10-14 14:21:15 +0300

    Update integration.js

--- integration.js
+++ integration.js
@@ -585,8 +585,12 @@ myEvent.watch(function (err, result) {
 });
 token1 = web3.eth.contract(tokenABI).at(token1Add);
 	token2 = web3.eth.contract(tokenABI).at(token2Add);
-	$( "." + token1Add).html(token1.symbol.call());
-	$( "." + token2Add).html(token2.symbol.call());
+	$( "." + token1Add).html(token1.symbol.call(function(error, result){
+		return result;
+	});
+	$( "." + token2Add).html(token2.symbol.call(function(error, result){
+		return result;
+	});
 
 //$( "div" ).html( "<span class='red'>Hello <b>Again</b></span>" );
 
