18d594ef2989c7ebfcbfdf0594ea69be89941b88
AlexxCh <sancho.ventura95@gmail.com>
2019-10-14 16:47:55 +0300

    Update integration.js

--- integration.js
+++ integration.js
@@ -581,12 +581,12 @@ myEvent.watch(function (err, result) {
 		token2Add = '' + result.args.takenTokenAddress;
 		var string = '<tr><td>' + result.args.maker + '</td>';
 		string += '<td class ="' + result.args.makerTokenAddress + '">';
-		var token1 = web3.eth.contract(tokenABI).at(token1Add);
+		/*var token1 = web3.eth.contract(tokenABI).at(token1Add);
 //	token2 = web3.eth.contract(tokenABI).at(token2Add);
 	string += token1.symbol.call(function(error, result){
 		console.log(result);
 		return result;
-	});
+	});*/
 	string += '</td><td>';
 		//string += await token1.symbol.call() + '</td><td>';
 		string += result.args.givenTokenAmount + '</td>';
