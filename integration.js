9a573a9dea37e9ae6fb96f99340b31bcfbe2a034
AlexxCh <sancho.ventura95@gmail.com>
2019-10-15 18:33:43 +0300

    Update integration.js

--- integration.js
+++ integration.js
@@ -636,7 +636,7 @@ function trade(hash) {
 	})) taken.approve('0x8e7c770cba5cbb342880e57fada571fdbefc0691', exchange.orderHashList.call(hash, function (err, result) {
 		return result.takenTokenAmount;
 	}), {from: web3.eth.accounts[0]});*/
-	taken.allowance.call(web3.eth.accounts[0], '0x8e7c770cba5cbb342880e57fada571fdbefc0691', async function (err, result) {
+	taken.allowance.call(web3.eth.accounts[0], '0x8e7c770cba5cbb342880e57fada571fdbefc0691', function (err, result) {
 		if (result.c[0] < amount) taken.approve('0x8e7c770cba5cbb342880e57fada571fdbefc0691', amount, function (err, result) {
 		})
 		//.then(exchange.trade(hash, {from: web3.eth.accounts[0]}));
