534d700256d1a7c86140a5c3abacf5b9ebc6ef3d
AlexxCh <sancho.ventura95@gmail.com>
2019-10-15 17:44:48 +0300

    Update integration.js

--- integration.js
+++ integration.js
@@ -627,7 +627,9 @@ myEvent.watch(function (err, result) {
 function trade(hash) {
 	exchange.orderHashList.call(hash, function (err, result) {
 		var add = result.takenTokenAddress;
+		console.log(result);
 	let taken = web3.eth.contract(tokenABI).at(add);
+	console.log(taken);
 	/*if (taken.allowance.call(web3.eth.accounts[0], '0x8e7c770cba5cbb342880e57fada571fdbefc0691', function(err, result) {
 		console.log(result);
 		console.log(result > 0);
