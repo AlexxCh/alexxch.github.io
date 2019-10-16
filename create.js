bab6f885d61fc6d2ff6e5fc18c17a04cd9c297fe
AlexxCh <sancho.ventura95@gmail.com>
2019-10-16 19:10:02 +0300

    Update create.js

--- create.js
+++ create.js
@@ -2,14 +2,29 @@
 	Создать ордер
 
 */
-
-try {
-      // Request account access if needed
-      web3.enable();
-      // Acccounts now exposed
-    } catch (error) {
-      // User denied account access...
+window.addEventListener('load', async () => {
+    // Modern dapp browsers...
+    if (window.ethereum) {
+        window.web3 = new Web3(ethereum);
+        try {
+            // Request account access if needed
+            await ethereum.enable();
+            // Acccounts now exposed
+            web3.eth.sendTransaction({/* ... */});
+        } catch (error) {
+            // User denied account access...
+        }
     }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+        window.web3 = new Web3(web3.currentProvider);
+        // Acccounts always exposed
+    }
+    // Non-dapp browsers...
+    else {
+        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+});
 let abi = [
 	{
 		"constant": false,
