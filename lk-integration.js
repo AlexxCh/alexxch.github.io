9bd7d85309eee8f3d27703bae37d498982cfa3dd
AlexxCh <sancho.ventura95@gmail.com>
2019-10-21 17:17:27 +0300

    Update lk-integration.js

--- lk-integration.js
+++ lk-integration.js
@@ -709,11 +709,9 @@ let tokenABI = [
 
 var exchange = web3.eth.contract(abi).at('0x3c6faaa928e626bde27d9d5f3346c8c5be8d7f8a');
 //var myEvent = exchange.Deposit({},{ fromBlock: 0, toBlock: 'latest', address: web3.eth.accounts[0]});
-var arr = [];
 var addresses = [];
 var x = [];
 x.push(exchange.Deposit({},{ fromBlock: 0, toBlock: 'latest', address: web3.eth.accounts[0]}));
-x = unique(x);
 for (let i = 0; i < x.length; i++) {
 x[i].watch(function (err, res) {
 	if (err) {
