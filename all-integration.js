3c820731dda49c2693806356ad7d690317c48ab5
AlexxCh <sancho.ventura95@gmail.com>
2019-10-24 15:39:25 +0300

    Update all-integration.js

--- all-integration.js
+++ all-integration.js
@@ -739,7 +739,7 @@ myEvent.watch(function (err, res) {
 		else  {
 			exchange.orderFilled(res.args.orderHash, function (err, r) {
 				console.log(r);
-				if (r == result[2].c[0]) {
+				if (r.c[0] == result[2].c[0]) {
 					var string = $('tbody').html();
 					string += '<tr class="table-success"><td>' + result[0] + '</td><td class="' + result[1] + '"></td><td>' + result[2].c[0] + '</td><td class="' + result[3] + '"></td><td>' + result[4].c[0] + '</td><td>Исполнено</td><td>' + res.args.orderHash + '</td>';
 					string += '<td></td></tr>';
